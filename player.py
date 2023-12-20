25cbfcf7a67e5ad1b5fde765077683ae3f50e136
Raphaël Fontaine <raphael.fontaine@mail.mcgill.ca>
2023-12-19 23:27:59 -0500

    Remove decimals

--- player.py
+++ player.py
@@ -2,11 +2,11 @@ from settings import *
 
 class Player():
     def __init__(self):
-        self.balance = 1000.00
-        self.bet_size = 100.00
-        self.last_payout = 0.00
-        self.total_won = 0.00
-        self.total_wager = 0.00
+        self.balance = 1000
+        self.bet_size = 100
+        self.last_payout = 0
+        self.total_won = 0
+        self.total_wager = 0
 
     def get_data(self):
         player_data = {}
